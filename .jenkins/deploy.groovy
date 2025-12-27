a01d464d0dd28dadb85cd43d9ab83288eb59aad2
Andrei Goncharov <andrei.goncharov@gcore.lu>
2025-12-27 23:29:27 +0200

    fixed deploy.groovy 9

--- .jenkins/deploy.groovy
+++ .jenkins/deploy.groovy
@@ -73,7 +73,7 @@ pipeline {
             steps {
                 sshagent(credentials: [env.RUNTIME_SSH_CRED]) {
                     sh """
-                    rsync -av docker-compose.yaml build/ ${REMOTE_USER}@${REMOTE_HOST}:/opt/app/
+                    rsync -av docker-compose.yaml build/ ${REMOTE_USER}@${REMOTE_HOST}:/home/runtime/app
 
                     ssh ${REMOTE_USER}@${REMOTE_HOST} '
                       cd /opt/app
