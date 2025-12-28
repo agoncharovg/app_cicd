6aabb953fe55aced0d85bd9527c4c6b63730f8a1
Andrei Goncharov <andrei.goncharov@gcore.lu>
2025-12-28 19:32:28 +0200

    fixed deploy.groovy 27

--- .jenkins/deploy.groovy
+++ .jenkins/deploy.groovy
@@ -73,7 +73,13 @@ pipeline {
             steps {
                 sshagent(credentials: [env.RUNTIME_SSH_CRED]) {
                     sh """
-                    rsync -av docker-compose.yaml test.env ${REMOTE_USER}@${REMOTE_HOST}:/home/runtime/app/
+                    rsync -av \
+                      build/ \
+                      src/ \
+                      manage.py \
+                      docker-compose.yaml \
+                      test.env \
+                      ${REMOTE_USER}@${REMOTE_HOST}:/home/runtime/app/
 
                     ssh ${REMOTE_USER}@${REMOTE_HOST} '
                       cd /home/runtime/app
